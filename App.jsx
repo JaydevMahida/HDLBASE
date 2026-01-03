61617d59489ccf9ba42713ebdee37df1abfff626
Dreamer-gc <yrathva2096@gmail.com>
2026-01-03 06:04:29 +0000

    made changes to arrangements

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "../pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
