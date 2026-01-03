d1a4f1484c578c9c929a7ddc453777c58a045403
Dreamer-gc <yrathva2096@gmail.com>
2026-01-03 12:59:11 +0000

    edited pages

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,11 +1,30 @@
-import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import "../styles/home.css";
+import hero from "../assets/hero.png";
 
 export default function Home() {
   return (
-    <div className="hero">
-      <h1>ELECTRONICS ENGINEERS</h1>
-      <p>PRACTICE Hardcore HDL Problems</p>
-      <Link to="/signup" className="btn">Practice HDL</Link>
-    </div>
+    <>
+      <Navbar />
+
+      <section className="hero">
+        <div className="hero-left">
+          <p className="subtitle">A PLATFORM FOR</p>
+          <h1>
+            ELECTRONICS<br />ENGINEERS
+          </h1>
+          <p className="desc">
+            Practise hardcore HDL problems
+          </p>
+          <button className="cta">PRACTISE HDL</button>
+        </div>
+
+        <div className="hero-right">
+          <div className="circle">
+            <img src={hero} alt="electronics" />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home.jsx";
-import Login from "./pages/Login.jsx";
-import Signup from "./pages/Signup.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+import Home from "../pages/home.jsx";
+import Login from "../pages/Login.jsx";
+import Signup from "../pages/Signup.jsx";
+import Dashboard from "../pages/Dashboard.jsx";
 
 export default function App() {
   return (
