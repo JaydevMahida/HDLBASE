7c0bbc28e28bdeda84b1c6c37191d683963656ef
Dreamer-gc <animewatch2958@gmail.com>
2026-01-03 20:22:23 +0000

    upd

--- components/Login.tsx
+++ components/Login.tsx
@@ -131,7 +131,7 @@ const Login: React.FC<Props> = ({ onLoginSuccess }) => {
               onClick={() => handleMockLogin(UserRole.PRODUCER)}
               className="text-[9px] py-3 bg-producer/5 border border-producer/20 text-producer rounded-xl hover:bg-producer/10 uppercase font-black tracking-widest transition-all"
             >
-              Mock Prod
+              Mock Producer
             </button>
             <button
               onClick={() => handleMockLogin(UserRole.CONSUMER)}
