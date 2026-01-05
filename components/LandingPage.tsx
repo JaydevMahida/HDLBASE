bb03816ad755426b47039bceb267c0761d4f2eda
Dreamer-gc <animewatch2958@gmail.com>
2026-01-05 13:57:53 +0000

    logo

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -229,7 +229,7 @@ const LandingPage: React.FC<Props> = ({ currentUser, onSignOut }) => {
             onClick={handleLogoClick}
             className="flex items-center cursor-pointer"
           >
-            <span className="font-black tracking-tighter text-lg">HDLBase</span>
+            <span className="font-black tracking-tighter text-lg"><img src={logo} alt="LOGO" className="w-32 h-auto mx-auto block"/></span>
           </div>
           <div className="flex gap-12 text-[10px] font-black uppercase tracking-widest text-gray-600">
             <a href="#" className="hover:text-white transition-colors">Privacy</a>
